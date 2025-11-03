e86d35f5d1949de10180afaafb28017aa168d6fa
Laurent Scherrer <147433411+LordLp9000@users.noreply.github.com>
2025-11-03 14:28:51 +0100

    aufgabe 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	firstName := "Laurent"
+	lastName := "Scherrer"
+	dayOfBirth := 9
+	monthOfBirth := 4
+	yearOfBirth := 2005
+	numberOfSiblings := 1
+	heightInMeters := 1.72
+	zodiacSign := '♉'
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
