0477433641c0bb03c87bf44376256d5984495331
Laurent Scherrer <147433411+LordLp9000@users.noreply.github.com>
2025-11-17 14:21:33 +0100

    Aufgabe 1 Go2

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
 // TODO: declare a structure for birth date
+type BirthDate struct {
+	Day   int
+	Month int
+	Year  int
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	Name             FullName
+	Birth            BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
@@ -17,12 +26,22 @@ type Profile struct {
 func main() {
 	var me = Profile{
 		// TODO: set name and birth date information
-		NumberOfSiblings: 0,   // TODO: adjust
-		ZodiacSign:       ' ', // TODO: adjust
+		Name: FullName{
+			FirstName: "Laurent",
+			LastName:  "Scherrer",
+		},
+		Birth: BirthDate{
+			Day:   9,
+			Month: 4,
+			Year:  2005,
+		},
+		NumberOfSiblings: 2,   // TODO: adjust
+		ZodiacSign:       '♈', // TODO: adjust (Aries for April 9)
 	}
 	fmt.Println(me)
 
 	fmt.Println("Siblings Before:", me.NumberOfSiblings)
 	// TODO: imagine, you get a little brother or sister
+	me.NumberOfSiblings++
 	fmt.Println("Siblings After:", me.NumberOfSiblings)
 }
