f1a9b3ebb53dfbe0fa3b7ad5d5052b56ba54dd06
Laurent Scherrer <147433411+LordLp9000@users.noreply.github.com>
2025-11-24 14:12:21 +0100

    Go 3 Aufgabe 3

--- go-3-ex-3/main.go
+++ go-3-ex-3/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 const (
 	Lower = 1
 	Upper = 30
@@ -7,4 +9,30 @@ const (
 
 func main() {
 	// TODO: Implement FizzBuzz using a for loop from Lower to Upper.
+	for i := Lower; i <= Upper; i++ {
+		if i%15 == 0 {
+			fmt.Println("FizzBuzz")
+		} else if i%3 == 0 {
+			fmt.Println("Fizz")
+		} else if i%5 == 0 {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+
+	fmt.Println("\n--- Switch/Case Version ---")
+	// Bonus: switch/case implementation
+	for i := Lower; i <= Upper; i++ {
+		switch {
+		case i%15 == 0:
+			fmt.Println("FizzBuzz")
+		case i%3 == 0:
+			fmt.Println("Fizz")
+		case i%5 == 0:
+			fmt.Println("Buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
 }
